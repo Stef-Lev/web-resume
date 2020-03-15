87f9d67392d3f0dbb67a991233c77ce3ca17d11c
Stefanos Leventis <54413716+Stef-Lev@users.noreply.github.com>
2020-03-15 14:40:19 +0200

    Update app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -41,7 +41,7 @@ function showError(error) {
 
 
 function getWeather(latitude, longitude) {
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
     fetch(api)
         .then(response => {
@@ -61,7 +61,7 @@ function getWeather(latitude, longitude) {
 }
 
 function getCityWeather(city) {
-    let api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
 
     fetch(api)
         .then(response => {
